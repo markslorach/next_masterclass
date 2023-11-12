e00f570f28a6a04ef9c91824034e75511f9fdd9c
Mark Slorach <hello@markslorach.com>
2023-11-12 16:18:13 +0000

    update

--- app/components/NavBar.jsx
+++ app/components/NavBar.jsx
@@ -1,17 +1,22 @@
+import Image from "next/image";
 import Link from "next/link";
+import Logo from "./dojo-logo.png";
 
 export default function NavBar() {
   return (
-    <nav className="flex justify-between">
-      <div>
-        <Link href={"/"}>
-          <h1>Dojo Helpdesk</h1>
-        </Link>
-      </div>
-      <div className="flex gap-4">
-        <Link href={"/"}>Dashboard</Link>
-        <Link href={"/tickets"}>Tickets</Link>
-      </div>
+    <nav>
+      <Image
+        src={Logo}
+        alt="Dojo Helpdesk logo"
+        width={70}
+        quality={100}
+        placeholder="blur"
+      />
+      <Link href={"/"}>
+        <h1>Dojo Helpdesk</h1>
+      </Link>
+      <Link href={"/"}>Dashboard</Link>
+      <Link href={"/tickets"}>Tickets</Link>
     </nav>
   );
 }
